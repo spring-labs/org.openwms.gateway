d7473b4279df8b3793475a57c66944bbd94364e9
Heiko Scherrer <scherrer@openwms.org>
2017-07-13 22:57:19 +0200

    worked on getting Zuul running in Openshift
    Signed-off-by: Heiko Scherrer <scherrer@openwms.org>

--- src/main/java/org/openwms/gateway/ApiGatewayRunner.java
+++ src/main/java/org/openwms/gateway/ApiGatewayRunner.java
@@ -27,6 +27,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 /**
  * A ApiGatewayRunner.
@@ -35,6 +36,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication(scanBasePackageClasses = SolutionApp.class, scanBasePackages = "org.openwms")
 @EnableZuulProxy
+@EnableZuulServer
 @EnableAspects(propagateRootCause = true)
 @EnableDiscoveryClient
 public class ApiGatewayRunner {
