8cb3d193a3fd08777eddfd150d9890e284116323
Heiko Scherrer <scherrer@openwms.org>
2023-06-02 11:12:06 +0200

    updated file headers before release

--- src/main/java/org/openwms/gateway/ApiGatewayModuleConfiguration.java
+++ src/main/java/org/openwms/gateway/ApiGatewayModuleConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2022 the original author or authors.
+ * Copyright 2005-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package org.openwms.gateway;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import org.ameba.annotation.EnableAspects;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
--- src/main/java/org/openwms/gateway/CORSConfig.java
+++ src/main/java/org/openwms/gateway/CORSConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2020 the original author or authors.
+ * Copyright 2005-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,7 +51,7 @@ public class CORSConfig implements WebFluxConfigurer {
                 .pathMatchers("/actuator/**").permitAll()
                 .anyExchange().authenticated()
                 .and()
-                //.oauth2Login().authorizedClientRepository()
+                //.oauth2Login()
                 //.and()
                 .logout()
                     .logoutUrl("/logout")
